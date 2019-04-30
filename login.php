0e4143c0b32cb85d57d2d09fae5a9d453f816edf
Dev Web Talis <dev.web.talis@gmail.com>
2019-04-30 11:30:16 +0200

    adding signup form

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     require('inc/connect.php');
     require('inc/functions.php');
     $message = "Formulaire non soumis!";
-    if( isset( $_POST['submit'] ) ){
+    if( isset( $_POST['submit-login'] ) ){
         $user_email =  $_POST['user_email']; //username issu du formulaire
         $user_password =  $_POST['user_password']; // password issu du formulaire
         global $mysqli;
@@ -22,7 +22,35 @@
             $message = "Identifiant inconnu.";
         }
     }
-
+    if( isset( $_POST['submit-signup'] ) ){
+        $user_email = $_POST['user_email_signup'];
+        $user_pass = $_POST['user_password_signup'];
+        $user_pass_2 = $_POST['user_password_2_signup'];
+        if( $res = $mysqli->query("SELECT * FROM users WHERE email_user = '".$user_email."' LIMIT 1") ){
+            $compteur = $res->num_rows;
+            if($compteur != 0){
+                $message = "Vous possédez déjà un compte.";
+            }else{
+                if( $user_pass == $user_pass_2 ){
+                // Enregistrer le compte
+                $add_user_query = "INSERT INTO `users`(
+                    `email_user`,
+                    `password_user`) 
+                    VALUES (
+                        '$user_email',
+                        '$user_pass'
+                    )";
+                    if( $res = $mysqli->query($add_user_query) ){
+                        $message ="Votre compte a bien été créé, vous pouvez maintenant vous enregistrer!";
+                    }
+                }else{
+                    $message = "Les mots de passe ne correspondent pas!";
+                }
+            }
+        }else{
+            $message = "Une erreur vient de se produire.";
+        }
+    }
     require('inc/head.php');
     include('inc/nav.php');
 ?>
@@ -46,12 +74,27 @@
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
-                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" name="submit-login" class="btn btn-primary">Submit</button>
                 </form>
             </div>
             <div class="col-md-6">
                 <h1>Signup Form</h1>
-                
+                <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="text" name="user_email_signup" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" name="user_password_signup" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password Verify</label>
+                        <input type="password" name="user_password_2_signup" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <button type="submit" name="submit-signup" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     </div>
