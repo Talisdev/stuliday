15a9e20191aa67d2848fd38f45ae88701999ebda
Dev Web Talis <dev.web.talis@gmail.com>
2019-04-30 15:57:01 +0200

    update links in nav

--- inc/nav.php
+++ inc/nav.php
@@ -4,27 +4,27 @@
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="navbar-brand" href="#">Stuliday</a>
+    <a class="navbar-brand" href="index.php">Stuliday</a>
     <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Annonces</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Mon compte</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link" href="annonces.php">Annonces</a>
       </li>
       <?php
       if( isset($_SESSION['login_email']) ){
       ?>
         <li class="nav-item">
-          <a class="nav-link" href="?logout">Log Out</a>
+          <a class="nav-link" href="profile.php">Mon compte</a>
         </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?logout">Log Out</a>
+          </li>
       <?php
+      }else{
+        ?>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Log In / Sign In</a>
+          </li>
+        <?php
       }
       ?>
     </ul>
